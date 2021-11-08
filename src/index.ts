8e3ea68f80967aff783ab105d20d1018e5b638be
jaluik <cqzhengmouren@gmail.com>
2021-11-08 20:11:46 +0800

    feat: no meaning commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,38 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('dot-log.helloWorld', () => {
-    vscode.window.showInformationMessage('Hello World from Dot Log!');
-  });
+class GoCompletionItemProvider implements vscode.CompletionItemProvider {
+  public provideCompletionItems(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    token: vscode.CancellationToken
+  ): Thenable<vscode.CompletionItem[]> {
+    const text = document.getText(
+      new vscode.Range(new vscode.Position(position.line, 0), position)
+    );
 
-  context.subscriptions.push(disposable);
+    const data = text
+      .match(/([\S])*.log$/g)
+      ?.map((item) => item.replace('.log', ''));
+
+    if (data?.length) {
+      return Promise.resolve([
+        {
+          label: 'aa',
+        },
+      ]);
+    }
+    return Promise.resolve([]);
+  }
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  const options = vscode.languages.registerCompletionItemProvider(
+    'javascript',
+    new GoCompletionItemProvider(),
+    '.log'
+  );
+
+  context.subscriptions.push(options);
 }
 
 export function deactivate() {}
